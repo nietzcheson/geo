2e1f0d9699b36faf8f56c982b1f721196c342b8d
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-12-02 10:05:39 -0500

    Perimetro de búsqueda

--- app/app.js
+++ app/app.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
         var latitude = 21.192689;
         var longitude = -86.8213484;
 
-        var grade = 1;
-        var latGradeKm = 111.12; // One latitude grade
-        var km = 100;
+        var signs = ['-', '+'];
 
-        var xGrade = ((km*grade) / latGradeKm).toFixed(7);
+        var perimeterKM = 50;
+        var perimeterSearchKM = 20;
+
+        var perimeter = (perimeterKM / 111.12).toFixed(7);
+        var perimeterSearch = (perimeterSearchKM / 111.12).toFixed(7);
 
         var map = new GMaps({
                 el: '#map',
@@ -36,10 +38,10 @@ $(document).ready(function(){
                 }
         });
 
-        var latNorte = parseFloat(latitude) + parseFloat(xGrade);
-        var latSur = parseFloat(latitude) - parseFloat(xGrade);
-        var lngEste = parseFloat(longitude) + parseFloat(xGrade);
-        var lngOeste = parseFloat(longitude) - parseFloat(xGrade);;
+        var latNorte = parseFloat(latitude) + parseFloat(perimeter);
+        var latSur = parseFloat(latitude) - parseFloat(perimeter);
+        var lngEste = parseFloat(longitude) + parseFloat(perimeter);
+        var lngOeste = parseFloat(longitude) - parseFloat(perimeter);
 
         map.addMarker({
                 lat: latNorte,
@@ -103,29 +105,41 @@ $(document).ready(function(){
                 fillOpacity: 0.6
         });
 
-        //
-        var max = 0.0099999;
-        //
-        var signs = ['-', '+'];
-        //
-        // var map = new GMaps({
-        //         el: '#map',
-        //         lat: latitude,
-        //         lng: longitude
-        // });
-        //
-        // GMaps.geolocate({
-        //         success: function(position){
-        //                 map.setCenter(latitude, longitude)
-        //         }
-        // });
-        //
-        // map.drawOverlay({
-        //         lat: latitude,
-        //         lng: longitude,
-        //         content: '<div class="overlay">Me</div>'
-        // });
-        //
+        var latNorteSearch = parseFloat(latitude) + parseFloat(perimeterSearch);
+        var latSurSearch = parseFloat(latitude) - parseFloat(perimeterSearch);
+        var lngEsteSearch = parseFloat(longitude) + parseFloat(perimeterSearch);
+        var lngOesteSearch = parseFloat(longitude) - parseFloat(perimeterSearch);
+
+        var path = [
+                [
+                        latSurSearch,
+                        longitude
+                ],
+                [
+                        latitude,
+                        lngEsteSearch
+                ],
+
+                [
+                        latNorteSearch,
+                        longitude
+                ],
+                [
+                        latitude,
+                        lngOesteSearch
+                ],
+
+        ];
+
+        polygon = map.drawPolygon({
+                paths: path, // pre-defined polygon shape
+                strokeColor: '#BBD8E9',
+                strokeOpacity: 1,
+                strokeWeight: 3,
+                fillColor: 'red',
+                fillOpacity: 0.7
+        });
+
         for (var i = 0; i < 100; i++) {
 
                 var sign = signs[Math.floor(Math.random() * signs.length)];
@@ -133,8 +147,8 @@ $(document).ready(function(){
                 if(sign == '-'){
                         //console.log('Menos: '+sign);
 
-                        var lat = (Math.random() * (0.00 - xGrade) + latitude).toFixed(7);
-                        var lng = (Math.random() * (0.00 + xGrade) + longitude).toFixed(7);
+                        var lat = (Math.random() * (0.00 - perimeter) + latitude).toFixed(7);
+                        var lng = (Math.random() * (0.00 + perimeter) + longitude).toFixed(7);
 
                         map.addMarker({
                                 lat: lat,
@@ -144,8 +158,8 @@ $(document).ready(function(){
                                 }
                         });
 
-                        var lat = (Math.random() * (0.00 + xGrade) + latitude).toFixed(7);
-                        var lng = (Math.random() * (0.00 - xGrade) + longitude).toFixed(7);
+                        var lat = (Math.random() * (0.00 + perimeter) + latitude).toFixed(7);
+                        var lng = (Math.random() * (0.00 - perimeter) + longitude).toFixed(7);
 
                         map.addMarker({
                                 lat: lat,
@@ -156,10 +170,9 @@ $(document).ready(function(){
                         });
 
                 }else if(sign == '+'){
-                        //console.log('Más: '+sign);
 
-                        var lat = (Math.random() * (0.00 + xGrade) + latitude).toFixed(7);
-                        var lng = (Math.random() * (0.00 + xGrade) + longitude).toFixed(7);
+                        var lat = (Math.random() * (0.00 + perimeter) + latitude).toFixed(7);
+                        var lng = (Math.random() * (0.00 + perimeter) + longitude).toFixed(7);
 
                         map.addMarker({
                                 lat: lat,
@@ -169,8 +182,8 @@ $(document).ready(function(){
                                 }
                         });
 
-                        var lat = (Math.random() * (0.00 - xGrade) + latitude).toFixed(7);
-                        var lng = (Math.random() * (0.00 - xGrade) + longitude).toFixed(7);
+                        var lat = (Math.random() * (0.00 - perimeter) + latitude).toFixed(7);
+                        var lng = (Math.random() * (0.00 - perimeter) + longitude).toFixed(7);
 
                         map.addMarker({
                                 lat: lat,
@@ -180,39 +193,6 @@ $(document).ready(function(){
                                 }
                         });
                 }
-
-                //console.log((Math.random() * (0.00 - max) + latitude).toFixed(7));
-                //console.log((Math.random() * (0.00 - max) + latitude).toFixed(7));
         }
-
-
-
-    //console.log(parseFloat(latitude) - parseFloat(0.001000));
-    //
-
-    //
-
-    //
-    // function getRandomInt(min, max) {
-    //     return Math.floor(Math.random() * (max - min + 1)) + min;
-    // }
-    //
-    //
-    // sum = 0.05;
-    //
-    // console.log((latitude + sum).toFixed(12));
-    //
-    // // for (var i = 0; i < 100; i++) {
-    // //
-    // //     latitudeRand = (Math.random() * (0.00 - sum) + sum).toFixed(7);
-    // //     longitudeRand = (Math.random() * (0.00 - sum) + sum).toFixed(7);
-    // //
-    // //     console.log(latitudeRand);
-    // //     console.log(longitudeRand);
-    // //
-
-    // //
-    // // }
-    //
 
 });
